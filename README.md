5c31daaf124ce3d1dcbfdf96b10b5199db2c4a03
Wy477 <88798840+Wy477-P@users.noreply.github.com>
2024-10-21 13:32:03 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# UDE
-A tool for nefarious activities.
+##################################
+## UDE Alpha v1.0 Oct 21st 2024 ##
+##################################
+--------------------------------------
+#/HOW TO USE/#
+To setup, just download the zip under releases in github. Then, extract the contents into whatever folder you'd like.
+For Windows users, download UDEwin.zip. For Mac users, download UDEmac.zip. For Linux users, I will eventually add a zip with my own personal scripts, but they require additional setup and a knowledge of bash to use.
+
+All you really gotta do to use things in this program is set up the config file to your liking. Try not to mess up the formatting since it may cause unexpected errors.
+
+For UDG, you can get a free API key from google's ai studio, and you can change other generation instructions as you please. Google AI Studio has some documentation for it if you are confused or want to add more settings in a fork.
+
+For UDP, you can change the delays to modify the speed at which it simulates typing, so you can either have it run at full speed or change it to be more similar to your own typing speed.
+
+For HOTKEYS, it explains itself. You can change key combos to whatever keys you like, and clicking those will run different things like pasting, killswitches, and calling the gemini api. This is VERY IMPORTANT! Without killswitches, it is very hard to close these programs, especially UDP!
+
+To start up the program, just run the executable file and it will stay open in the background, waiting for you to press any hotkeys. The config file contains all the hotkeys as well, so you can change them if you want.
+
+To update the hotkeys/config, you need to press the default keybind for it which is also in the config folder. You can change it if you wish though. You can also update the config/hotkeys by just closing out the program and opening it again.
+
+I will eventually add visual icons to show what module is running, but for now that won't be the case.
+#/HOW TO USE/#
+--------------------------------------
+#/ABOUT/#
+UDE is a windows port for all my personal scripts that I use for nefarious actions (cheating). I will eventually post the bash versions of the scrips for people who use linux.
+
+UDG calls google's gemini api and uses your copied text as the prompt, then sends the result back to you clipboard completely undetected.
+
+UDP types out what is in your clipboard like a human would.
+#/ABOUT/#
+--------------------------------------
+#/DEBUGGING/#
+The logs folder will contain most information being sent across the program, so you can easily see the what's going on and what errors are popping up. 
+
+As long as you don't mess up the formatting of the cfg files or context json then you shouldn't ever need to check the logs.
+#/DEBUGGING/#
+--------------------------------------
---
+++ UDE.py
@@ -0,0 +1,200 @@
+import os
+import configparser
+import json
+import pyperclip
+import keyboard
+from functools import partial
+import requests
+import pyautogui
+import time
+import random
+import sys
+
+# DIRECTORIES #
+workdir = os.getcwd()
+
+def folfind(fo):
+    return os.path.join(workdir, fo)
+
+def filfind(fo, fi):
+    return os.path.join(fo, fi)
+
+logdir = folfind("logs")
+
+configini = filfind(workdir,"config.ini")
+gcontextjson = filfind(logdir,"gcontext.json")
+goutput = filfind(logdir,"goutput.txt")
+# DIRECTORIES #
+
+
+
+# EXIT FUNCTION #
+def closeude():
+    keyboard.unhook_all_hotkeys()
+    sys.exit()
+# EXIT FUNCTION #
+
+
+
+# UPDATE CONTEXT FUNCTION #
+def ac(t, r):
+    try:
+        con = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        con = []
+    with open(gcontextjson, 'r') as f:
+        con = json.load(f)
+    e = {'role': r, 'parts': [{'text': t}]}
+    con.append(e)
+    with open(gcontextjson, 'w') as f:
+        json.dump(con, f)
+# UPDATE CONTEXT FUNCTION #
+
+
+
+# UDG FUNCTION #
+def UDG(key, inst, temper, toppp, topkk):
+    if not os.path.isfile(gcontextjson):
+        with open(gcontextjson, 'w') as f:
+            json.dump([], f)
+    userin = pyperclip.paste()
+    user = "user"
+    ac(userin, user)
+    with open(gcontextjson, 'r') as f:
+        con = json.load(f)
+    SN = {
+        "system_instruction": {
+            "parts": [{
+                "text": inst
+            }]
+        },
+        "safetySettings": [
+            {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
+            {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
+        ],
+        "generationConfig": {
+            "temperature": temper,
+            "topP": toppp,
+            "topK": topkk
+        },
+        "contents": con
+    }
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8b:generateContent?key={key}"
+    response = requests.post(url, json=SN, headers={'Content-Type': 'application/json'})
+    RP = response.json()
+    OP = RP.get('candidates', [{}])[0].get('content', {}).get('parts', [{}])[0].get('text', '')
+    pyperclip.copy(OP)
+    ac(OP, "model")
+    with open(goutput, 'w') as file:
+        json.dump(RP, file)
+# UDG FUNCTION #
+
+
+
+# UDP FUNCTION #
+running = True
+def UDP(delay_per_key, word_delay_min, word_delay_max):
+    global running
+    running = True
+    text = pyperclip.paste()
+    text = text.lstrip()  
+    
+    time.sleep(0.1)
+    
+    for char in text:
+        if not running:
+            break
+        if char == '’':
+            pyautogui.typewrite("'")
+        else:
+            if char.isupper() or char in [':', '"', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '|', '<', '>', '?', '~']:
+                pyautogui.keyDown('shift')
+                pyautogui.typewrite(char)
+                pyautogui.keyUp('shift')
+            else:
+                pyautogui.typewrite(char)
+
+        time.sleep(delay_per_key)
+        
+        if char == ' ':
+            word_delay = random.uniform(word_delay_min, word_delay_max)
+            time.sleep(word_delay)
+# UDP FUNCTION #
+
+
+
+# UDP KILLSWITCH #
+def kill_switch():
+    global running
+    running = False
+# UDP KILLSWITCH #
+
+
+
+# CLEAR CONTEXT #
+def clearcon():
+    if os.path.isfile(gcontextjson):
+        os.remove(gcontextjson)
+    with open(gcontextjson, 'w') as f:
+        json.dump([], f)
+# CLEAR CONTEXT #
+
+
+
+# CHECK & IMPORT CONFIGS & UPDATE HOTKEYS #
+apikey = None
+sysin = None
+temp = None
+topp = None
+topk = None
+dpk = None
+wdmin = None
+wdmax = None
+EXIT = None
+CALLUDG = None
+CALLUDP = None
+CLEARCONTEXT = None
+UPDATECONFIG = None
+KILLSWITCH = None
+def updatecfg(relod):
+    global apikey, sysin, temp, topp, topk, dpk, wdmin, wdmax, EXIT, CALLUDG, CALLUDP, CLEARCONTEXT, UPDATECONFIG
+    config = configparser.ConfigParser()
+    config.read(configini)
+    
+    apikey = config['UDG']['api_key']
+    sysin = config['UDG']['system_instructions']
+    temp = config['UDG']['temperature']
+    topp = config['UDG']['top_p']
+    topk = config['UDG']['top_k']
+    dpk = config['UDP']['delay_per_key']
+    wdmin = config['UDP']['word_delay_min']
+    wdmax = config['UDP']['word_delay_max']
+    EXIT = config['HOTKEYS']['exit_hotkey']
+    CALLUDG = config['HOTKEYS']['udg_hotkey']
+    CALLUDP = config['HOTKEYS']['udp_hotkey']
+    CLEARCONTEXT = config['HOTKEYS']['clear_context']
+    UPDATECONFIG = config['HOTKEYS']['update_config']
+    KILLSWITCH = config['HOTKEYS']['udp_killswitch']
+    
+    if relod:
+        keyboard.remove_hotkey(EXIT)
+        keyboard.remove_hotkey(CALLUDG)
+        keyboard.remove_hotkey(CALLUDP)
+        keyboard.remove_hotkey(CLEARCONTEXT)
+        keyboard.remove_hotkey(UPDATECONFIG)
+        keyboard.remove_hotkey(KILLSWITCH)
+    
+    keyboard.add_hotkey(EXIT, closeude)
+    keyboard.add_hotkey(CALLUDG, partial(UDG, apikey, sysin, temp, topp, topk))
+    keyboard.add_hotkey(CALLUDP, partial(UDP, dpk, wdmin, wdmax))
+    keyboard.add_hotkey(CLEARCONTEXT, clearcon)
+    keyboard.add_hotkey(UPDATECONFIG, partial(updatecfg, True))
+    keyboard.add_hotkey(KILLSWITCH, kill_switch)
+updatecfg(False)
+# CHECK & IMPORT CONFIGS & UPDATE HOTKEYS #
+
+
+
+keyboard.wait()
